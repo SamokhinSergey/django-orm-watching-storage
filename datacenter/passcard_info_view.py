b0c862026fd23f2430fe129b65603db16410cbfc
SERGEY SAMOKHIN <samokhin@live.ru>
2022-05-15 16:17:02 +0400

    Code optimisation

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,26 +1,26 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from datacenter.time_tools import duration_format, duration_check
+from datacenter.time_tools import format_duration, check_suspicious
 
 
 def passcard_info_view(request, passcode):
-    passcards = Passcard.objects.filter(passcode=passcode)
-    visits_serialized = Visit.objects.filter(passcard=passcards[0])
-    user_visits = []
-    for visit in visits_serialized:
-        duration_time = duration_format(visit.get_duration())
+    passcards = Passcard.objects.filter(passcode=passcode)[0]
+    visits= Visit.objects.filter(passcard=passcards)
+    visits_serialized = []
+    for visit in visits:
+        duration_time = format_duration(visit.get_duration())
         duration_hours = duration_time['hours']
         duration_minutes = duration_time['minutes']
         this_passcard_visits = {
                 'entered_at': visit.entered_at,
                 'duration': f"{duration_hours }ч {duration_minutes}м",
-                'is_strange': duration_check(duration_hours, duration_minutes),
+                'is_strange': check_suspicious(duration_hours, duration_minutes),
             }
-        user_visits.append(this_passcard_visits)
+        visits_serialized.append(this_passcard_visits)
 
     context = {
-        'passcard': passcards[0],
-        'this_passcard_visits': user_visits
+        'passcard': passcards,
+        'this_passcard_visits': visits_serialized
     }
     return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.utils.timezone import localtime
-from datacenter.time_tools import duration_format, duration_check
+from datacenter.time_tools import format_duration, check_suspicious
 from datacenter.models import Visit
 
 
@@ -10,14 +10,14 @@ def storage_information_view(request):
     visitors_in_storage = []
     for index, active_visitor in enumerate(visits_serialized):
         entried_msk = localtime(visits_serialized[index].entered_at)
-        duration_time = duration_format(visits_serialized[index].get_duration())
+        duration_time = format_duration(visits_serialized[index].get_duration())
         duration_hours = duration_time['hours']
         duration_minutes = duration_time['minutes']
         visitor_in_sorage = {
             'who_entered': visits_serialized[index].passcard,
             'entered_at': entried_msk,
             'duration': f"{duration_hours }ч {duration_minutes}м",
-            'is_strange': duration_check(duration_hours, duration_minutes),
+            'is_strange': check_suspicious(duration_hours, duration_minutes),
         }
         visitors_in_storage.append(visitor_in_sorage)
     context = {
