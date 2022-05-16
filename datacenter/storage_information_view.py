6f8872330e8cd1647d1fa10fa3906930dde55143
SERGEY SAMOKHIN <samokhin@live.ru>
2022-05-16 15:30:52 +0400

    variables name optimisation

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -5,22 +5,21 @@ from datacenter.models import Visit
 
 
 def storage_information_view(request):
-
-    visitors_in_storage = Visit.objects.filter(leaved_at__isnull=True)
-    visits_serialized = []
-    for index, active_visitor in enumerate(visitors_in_storage):
-        entried_msk = localtime(visitors_in_storage[index].entered_at)
-        duration_time = format_duration(visitors_in_storage[index].get_duration())
+    storage_visits = Visit.objects.filter(leaved_at__isnull=True)
+    storage_visits_serialized = []
+    for index, active_visitor in enumerate(storage_visits):
+        entried_msk = localtime(storage_visits[index].entered_at)
+        duration_time = format_duration(storage_visits[index].get_duration())
         duration_hours = duration_time['hours']
         duration_minutes = duration_time['minutes']
         visitor_in_sorage = {
-            'who_entered': visitors_in_storage[index].passcard,
+            'who_entered': storage_visits[index].passcard,
             'entered_at': entried_msk,
             'duration': f"{duration_hours}ч {duration_minutes}м",
             'is_strange': check_suspicious(duration_hours, duration_minutes),
         }
-        visits_serialized.append(visitor_in_sorage)
+        storage_visits_serialized.append(visitor_in_sorage)
     context = {
-        'non_closed_visits': visits_serialized,
+        'non_closed_visits': storage_visits_serialized,
     }
     return render(request, 'storage_information.html', context)
